ef712e795e9e1555ec0b7193d4e528eccf897092
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 04:05:18 -0400

    added 3.py -print

---
+++ 3.py
@@ -0,0 +1,6 @@
+rate_of_doller=float(input("current rate of 1 doller in Indian rupies"))
+print(rate_of_doller)
+doller_value=input("enter the doller value to convert in indian rupies")
+print(doller_value)
+Indian_value_of_doller=(rate_of_doller)*(rate_of_doller)
+print("indian value of doller",Indian_value_of_doller)
