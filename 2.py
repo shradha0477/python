48721a876220ea215a717c7b39c78ab130b4d665
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:21:54 -0400

    added 2.py -print

--- 2.py
+++ 2.py
@@ -7,10 +7,16 @@
 #print(type(a))
 #print(id(a))
 
-b=list()
+#b=list()
+#print(b)
+#print(type(b))
+#print(id(b))
+#b.append(100)
+#print(b)
+#print(id(b))
+
+a=input("enter the age")
+
+print(a)
+b=int(a)-10
 print(b)
-print(type(b))
-print(id(b))
-b.append(100)
-print(b)
-print(id(b))
