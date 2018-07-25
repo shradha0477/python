437ad23e51b7ab0c88f4679f3e5121c335da1b9d
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:10:38 -0400

    added 2.py -print

--- 2.py
+++ 2.py
@@ -1,9 +1,16 @@
-a=2.0
-print(a)
-print(type(a))
-print(id(a))
+#print(a)
+#print(type(a))
+#print(id(a))
 
-a=a+10
-print(a)
-print(type(a))
-print(id(a))
+#a=a+10
+#print(a)
+#print(type(a))
+#print(id(a))
+
+b=list()
+print(b)
+print(type(b))
+print(id(b))
+b.append(100)
+print(b)
+print(id(b))
